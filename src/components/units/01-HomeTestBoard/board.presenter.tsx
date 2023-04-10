c9010ae52bdd67d3c70644ed165ac9cf05a235f0
kyungmin kim <ee5201@naver.com>
2023-04-11 00:43:27 +0900

    updatestudy

--- src/components/units/01-HomeTestBoard/board.presenter.tsx
+++ src/components/units/01-HomeTestBoard/board.presenter.tsx
@@ -3,72 +3,75 @@ import * as S from "./board.styles";
 export default function BoardPresenter() {
   return (
     <S.Wrapper>
-      <h1>게시물 등록하기</h1>
+      <S.MainTitle>게시물 등록하기</S.MainTitle>
 
-      <div>
-        <div>
-          <span>작성자:</span>
-          <input type="text" />
-        </div>
-        <div>
-          <span>비밀번호:</span>
-          <input type="password" />
-        </div>
-      </div>
+      <S.BoardWriterWrapper>
+        <S.BoardWriterinputBox>
+          <S.Lable>작성자:</S.Lable>
+          <S.BoardWriterinput type="text" />
+        </S.BoardWriterinputBox>
+        <S.BoardWriterinputBox>
+          <S.Lable>비밀번호:</S.Lable>
+          <S.BoardWriterinput type="password" />
+        </S.BoardWriterinputBox>
+      </S.BoardWriterWrapper>
 
-      <div>
-        <div>
-          <span>제목:</span>
-          <input type="text" />
-        </div>
-      </div>
+      <S.BoardTitleWrapper>
+        <S.BoardTitleinputBox>
+          <S.Lable>제목:</S.Lable>
+          <S.BoardTitleinput type="text" />
+        </S.BoardTitleinputBox>
+      </S.BoardTitleWrapper>
 
-      <div>
-        <div>
-          <span>내용:</span>
-          <input type="text" />
-        </div>
-      </div>
+      <S.BoardContentWrapper>
+        <S.BoardContentinputBox>
+          <S.Lable>내용:</S.Lable>
+          <S.BoardContentinput type="text" />
+        </S.BoardContentinputBox>
+      </S.BoardContentWrapper>
 
-      <div>
-        <span>주소</span>
-        <div>
-          <input type="text" />
-          <button>우편번호 검색</button>
-        </div>
-        <input type="text" />
-        <input type="text" />
-      </div>
+      <S.BoardZipWrapper>
+        <S.Lable>주소</S.Lable>
+        <S.BoardZipinputBox>
+          <S.BoardZipinput type="text" />
+          <S.BoardZipButton>우편번호 검색</S.BoardZipButton>
+        </S.BoardZipinputBox>
+        <S.BoardZipDetailinput type="text" />
+        <S.BoardZipDetailinput type="text" />
+      </S.BoardZipWrapper>
 
-      <div>
-        <span>유튜브:</span>
-        <input type="text" />
-      </div>
+      <S.BoardTitleWrapper>
+        <S.BoardYouTubeinputBox>
+          <S.Lable>유튜브:</S.Lable>
+          <S.BoardTitleinput type="text" />
+        </S.BoardYouTubeinputBox>
+      </S.BoardTitleWrapper>
 
-      <div>
-        <span>사진첨부</span>
-        <div>
-          <button>+</button>
-          <button>+</button>
-          <button>+</button>
-        </div>
-      </div>
+      <S.BoardZipWrapper>
+        <S.Lable>사진첨부</S.Lable>
+        <S.BoardImageButtonBox>
+          <S.BoardImageButton>+</S.BoardImageButton>
+          <S.BoardImageButton>+</S.BoardImageButton>
+          <S.BoardImageButton>+</S.BoardImageButton>
+        </S.BoardImageButtonBox>
+      </S.BoardZipWrapper>
 
-      <div>
-        <span>메인 설정</span>
-        <div>
-          <input type="radio" />
-          <span>유튜브</span>
-        </div>
-        <div>
-          <input type="radio" />
-          <span>사진</span>
-        </div>
-      </div>
-
-      <div>
-        <button>등록하기</button>
-      </div>
+      <S.BoardZipWrapper>
+        <S.Lable>메인설정</S.Lable>
+        <S.BoardImageButtonBox>
+          <S.BoardImageButtonBox>
+            <S.BoardMainSettinginput type="radio" />
+            <S.Lable>유튜브</S.Lable>
+          </S.BoardImageButtonBox>
+          <S.BoardImageButtonBox>
+            <S.BoardMainSettinginput type="radio" />
+            <S.Lable>사진</S.Lable>
+          </S.BoardImageButtonBox>
+        </S.BoardImageButtonBox>
+      </S.BoardZipWrapper>
+      <S.BoardSubmittWrapper>
+        <S.BoardWriteSubmit>등록하기</S.BoardWriteSubmit>
+      </S.BoardSubmittWrapper>
     </S.Wrapper>
   );
 }
--- src/components/units/01-HomeTestBoard/board.styles.tsx
+++ src/components/units/01-HomeTestBoard/board.styles.tsx
@@ -8,3 +8,160 @@ export const Wrapper = styled.div`
   margin: 100px;
   box-shadow: 0px 0px 10px gray;
 `;
+
+export const MainTitle = styled.h1`
+  display: flex;
+  padding-top: 60px;
+  justify-content: center;
+`;
+
+export const BoardWriterWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding-top: 80px;
+  justify-content: center;
+`;
+
+export const BoardWriterinputBox = styled.div`
+  margin-left: 16px;
+`;
+
+export const Lable = styled.span`
+  width: 59px;
+  height: 24px;
+  left: 971px;
+  top: 910px;
+
+  font-family: "Noto Sans CJK KR";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+`;
+
+export const BoardWriterinput = styled.input`
+  display: flex;
+  margin-top: 16px;
+  width: 486px;
+  height: 52px;
+`;
+
+export const BoardTitleWrapper = styled.div`
+  display: flex;
+  padding-top: 40px;
+  justify-content: center;
+`;
+
+export const BoardTitleinputBox = styled.div`
+  margin-left: 16px;
+`;
+
+export const BoardTitleinput = styled.input`
+  display: flex;
+  margin-top: 16px;
+  width: 988px;
+  height: 52px;
+`;
+
+export const BoardContentWrapper = styled.div`
+  display: flex;
+  padding-top: 40px;
+  justify-content: center;
+`;
+
+export const BoardContentinputBox = styled.div`
+  margin-left: 16px;
+`;
+
+export const BoardContentinput = styled.input`
+  display: flex;
+  margin-top: 16px;
+  height: 486px;
+  width: 988px;
+`;
+
+export const BoardZipWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 40px;
+  justify-content: center;
+  margin-left: 117px;
+`;
+
+export const BoardZipinputBox = styled.div`
+  margin-right: 16px;
+  display: flex;
+`;
+
+export const BoardZipinput = styled.input`
+  display: flex;
+  margin-top: 16px;
+  height: 52px;
+  width: 77px;
+  left: 461px;
+  top: 1802px;
+  border-radius: 0px;
+`;
+
+export const BoardZipButton = styled.button`
+  display: flex;
+  margin-top: 16px;
+  height: 52px;
+  width: 124px;
+  left: 461px;
+  top: 1802px;
+  border-radius: 0px;
+  padding: 14px 16px 14px 16px;
+  margin-left: 16px;
+`;
+
+export const BoardZipDetailinput = styled.input`
+  display: flex;
+  margin-top: 16px;
+  height: 52px;
+  width: 996px;
+`;
+
+export const BoardYouTubeinputBox = styled.div`
+  margin-left: 30px;
+`;
+
+export const BoardImageButtonBox = styled.div`
+  display: flex;
+`;
+
+export const BoardImageButton = styled.button`
+  height: 78px;
+  width: 78px;
+  left: 665px;
+  top: 2228px;
+  border-radius: 0px;
+  margin-right: 16px;
+`;
+
+export const BoardMainSettinginput = styled.input`
+  height: 20px;
+  width: 20px;
+  left: 2px;
+  top: 2px;
+  border-radius: 0px;
+  margin-right: 10px;
+`;
+
+export const BoardWriteSubmit = styled.button`
+  display: flex;
+  height: 52px;
+  width: 179px;
+  left: 870px;
+  top: 2412px;
+  border-radius: 0px;
+  padding: 14px 60px 14px 60px;
+  justify-content: center;
+`;
+
+export const BoardSubmittWrapper = styled.div`
+  display: flex;
+  padding-top: 40px;
+  justify-content: center;
+  margin-bottom: 114px;
+`;
